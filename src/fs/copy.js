97f0ec99502e36da837cfd6b3036171a4b4b7bc4
Sergey Kruptsov <serega.by@inbox.ru>
2022-06-01 11:53:25 +0300

    feat: add solution taks create, copy, rename

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,17 @@
+import * as fs from 'fs/promises'
+import * as path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+const PathFromCopy = (path.join(__dirname, 'files'));
+const PathToCopy = path.join(__dirname, 'files_copy');
+
 export const copy = async () => {
-    // Write your code here 
+    fs.readdir(PathFromCopy, { withFileTypes: true })
+    .then(fs.mkdir('files_copy'))
+    .then(() => fs.readdir(PathFromCopy, { withFileTypes: true }))
+    .then((data) => {data.forEach(e => fs.copyFile(`${PathFromCopy}/${e.name}`, `${PathToCopy}/${e.name}`))})
+    .catch((err) => {if(err)throw new error ('FS operation failed')})
 };
+copy()
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,15 @@
+import * as fs from 'fs/promises'
+import * as path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+const PathFromRename = (path.join(__dirname, 'files', 'wrongFilename.txt'));
+const PathToRename = path.join(__dirname, 'files', 'properFilename.md');
+
 export const rename = async () => {
-    // Write your code here 
+    fs.readFile(PathFromRename)
+    .then(fs.rename(PathFromRename, PathToRename))
+    .catch((err)=> {if(err) throw new Error ('FS operation failed')})
 };
+rename()
