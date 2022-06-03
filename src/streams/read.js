f4674d65b964f103e61669c73eb63b1739a47ca6
Sergey Kruptsov <serega.by@inbox.ru>
2022-06-03 10:37:30 +0300

    feat: add solution task stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,17 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url';
+const { stdout } = process;
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+const PathFromRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
 export const read = async () => {
-    // Write your code here 
+    let data = ''
+    const readData = fs.createReadStream(PathFromRead, 'utf-8')
+    readData.on('data', chunk => data += chunk);
+    readData.on('end', ()=> stdout.write(data))
+    readData.on('error', e => console.log(e))
 };
+read()
