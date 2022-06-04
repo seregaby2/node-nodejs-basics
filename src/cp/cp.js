f3130610b4cf68058aaad6ed470f0454cfb9c4fd
Sergey Kruptsov <serega.by@inbox.ru>
2022-06-04 23:48:04 +0300

    feat: add solution task cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,18 @@
+import child_process from 'child_process';
+import * as fs from 'fs/promises'
+import * as path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+const PathChild = path.join(__dirname, 'files', 'script.js');
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = child_process.fork(PathChild, args )
+    child.on('data', (data)=> {
+        process.stdin.write(data)
+    })
+
 };
+
+spawnChildProcess([4,2,3])
