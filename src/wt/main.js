86d789ac9a12bbddde46900af52fa62160058416
Sergey Kruptsov <serega.by@inbox.ru>
2022-06-05 16:42:41 +0300

    feat: add solution worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,33 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
 export const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const calculateNewData = (workerData) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker("./worker.js", { workerData });
+      worker.on("message", (message) => {
+        resolve(message);
+      });
+      worker.on("error", () => {
+        reject({ data: null, status: "error" });
+      });
+    });
+  };
+  
+  try {
+      const array = []
+    for (let i = 0; i < cpus().length; i++) {
+         array.push(i+10)   
+    }
+    const result = await Promise.all(
+        array.map(e=>calculateNewData(e))
+    );
+    return result;
+  } catch (e) {
+    console.error("worker error", e);
+  }
+  
 };
+
+performCalculations().then(data=>console.log(data))
